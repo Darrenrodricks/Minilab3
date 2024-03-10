b5d02ad36939ace389508d35c21fe47de4be1205
Darren Rodricks <86697301+Darrenrodricks@users.noreply.github.com>
2024-03-10 16:35:06 -0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "myclass.h"
+
+using namespace std;
+
+int main () {
+  cout << "Hello World, my name is Darren Rodricks, rah" << endl;
+
+  MyClass a(5);
+  cout << a.getX() << endl;
+}
